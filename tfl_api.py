0bdeba681c52d2cb12500c43ee9c0e2c3542a596
makiisthenes <52138450+makiisthenes@users.noreply.github.com>
2019-11-29 18:05:16 +0000

    Update tfl_api.py

--- tfl_api.py
+++ tfl_api.py
@@ -12,7 +12,7 @@ tfl_api_key = 'YOUR_API_KEY'
 init()
 
 
-while 1 != 0:
+while True:
     tfl1 = requests.get('https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=false&app_id=158d290e&app_key=6192c53aa8807df16ad4f00b76bf7b00').json()
     print(tfl1[0]['name']+ ' --> ' + tfl1[0]['lineStatuses'][0]['statusSeverityDescription'])
     print(tfl1[1]['name']+ ' --> ' + tfl1[1]['lineStatuses'][0]['statusSeverityDescription'])
